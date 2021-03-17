94ae62a053714ba0fd6106613572ba8164f5de15
davimarquesgiareta <62272924+davimarquesgiareta@users.noreply.github.com>
2021-03-17 20:14:53 -0300

    Add files via upload

---
+++ frontend/src/components/template/Content.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="content"> 
+      <div class="container-fluid">
+          <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Brands</th>
+      <th scope="col">Type</th>
+      <th scope="col">Price</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="guitar in guitars" :key="guitar.id">
+      <th scope="row">{{guitar.id}}</th>
+      <td>{{guitar.brands}}</td>
+      <td>{{guitar.type}}</td>
+      <td>R$ {{guitar.price}}</td>
+    </tr>
+    
+  </tbody>
+</table>
+
+      </div>
+    </div>
+     
+</template>
+
+<script>
+
+const axios = require('axios')
+
+export default {
+    name: "Content",
+    data(){
+        return {
+             guitars:[]
+        }
+    },
+
+    mounted(){
+        this.getGuitars()
+    },
+
+    methods: {
+       async getGuitars(){
+            const response = await axios.get('http://localhost:3000/guitars')
+        
+        if (response.status == 200){
+            this.guitars = response.data;
+        }else {
+            console.error("erro")
+        }
+        }
+    }
+}
+
+
+</script>
+
+<style>
+   .content {
+        grid-area: content;
+        background-color: #efefef;
+        padding: 20px;
+        
+    }
+</style>
---
+++ frontend/src/components/template/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  
+  <header class="header">
+      <div class="container-fluid" align="center">
+          <h2 id="titulopagina">The Best Guitars of the World</h2>
+      </div>
+      
+      </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style>
+
+    #titulopagina{
+        color: white;
+    }
+    .header {
+        grid-area: header;
+        background:linear-gradient(to right, #1e469a, #49a7c1);
+
+        display: flex;
+        justify-content: center;
+        align-items:center;
+    }
+</style>
